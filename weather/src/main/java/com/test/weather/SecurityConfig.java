2662d6f84c3c95a5216192be30b6fef0ed89cf71
Pratik Patil <114714806+Pratik22-Patil@users.noreply.github.com>
2024-05-01 19:56:28 +0530

    Add files via upload

---
+++ weather/src/main/java/com/test/weather/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.test.weather;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable();
+            // Use basic auth; can replace with custom authentication strategy
+
+        return http.build();
+    }
+}
